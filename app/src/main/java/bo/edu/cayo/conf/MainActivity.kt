3bd3a81138a876630e56c81d29e022a8f4b829f4
claudiomelendresnogales <claudio.melendres@incontact.com>
2020-12-07 01:50:32 -0400

    first

---
+++ app/src/main/java/bo/edu/cayo/conf/MainActivity.kt
@@ -0,0 +1,18 @@
+package bo.edu.cayo.conf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
---
+++ app/src/main/java/bo/edu/cayo/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,14 @@
+package bo.edu.cayo.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bo.edu.cayo.conf.R
+
+// carpta
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+
+}
